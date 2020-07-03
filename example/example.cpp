018c35c352e355031ee5b111519fa056a3ed6485
stearells <stearells@gmail.com>
2020-07-03 19:57:34 +0300

    little fix

--- example/example.cpp
+++ example/example.cpp
@@ -1,11 +1,14 @@
 ﻿#include "example.h"
 
 Detour test;
+
 typedef int (*fun)(int arg);
+fun orig;
+fun hooked;
 
 int originalFun(int arg)
 {
-	printf("Original: %d\n", arg);
+	printf("From original: %d\n", arg);
 	return arg + 1;
 }
 
@@ -13,16 +16,19 @@ int hookedFun(int arg)
 {
 	test.UnInstall();
 	printf("From hooked: %d\n", arg);
-	int rsl = originalFun(arg);
+	int rsl = orig(arg);
 	test.ReInstall();
 	return rsl;
 }
 
 int main(int argc, char* argv[])
 {
-	printf("Hello, this is testing application.\n");
+	printf("Hello, this is detours testing application.\n");
 
-	test.Install(reinterpret_cast<long*>((fun)originalFun), reinterpret_cast<long*>((fun)hookedFun));
+	orig = &originalFun;
+	hooked = &hookedFun;
+
+	test.Install(orig, hooked);
 	printf("Hooked result: %d", originalFun(5));
 	test.UnInstall();
 
